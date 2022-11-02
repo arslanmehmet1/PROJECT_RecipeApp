b8e0308b9631c9514a99160e2dcdc442776b88b7
Mehmet Arslan <marslan.developer@gmail.com>
2022-11-02 19:28:35 +0300

    2 day 1 lesson

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import {
+  Button,
+  FoodInput,
+  FormContainer,
+  HeaderContainer,
+  MainHeader,
+  Select,
+} from "./Header.style";
 
-const Header = () => {
+const Header = ({ setQuery, setSelectedMeal, getData }) => {
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>RecipeApp</MainHeader>
 
-export default Header
+      <FormContainer onSubmit={handleSubmit}>
+        <FoodInput
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button type="submit">SEARCH</Button>
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealType.map((meal, index) => (
+            <option key={index} value={meal}>
+              {meal}
+            </option>
+          ))}
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
+  const APP_ID = "673a8dfd";
+  const APP_KEY = "461f481233851d0b4d0a3797cf24d7b3";
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState([]);
+  // const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    const { data } = await axios(url);
+    setRecipes(data.hits);
+    console.log(recipes);
+  };
+  console.log(recipes);
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
   return (
     <div>
-      Home
+      <Header
+        setQuery={setQuery}
+        setSelectedMeal={setSelectedMeal}
+        getData={getData}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
