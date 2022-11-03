c161e5b649102e662a0de944c764f304ff25ec86
Mehmet Arslan <marslan.developer@gmail.com>
2022-11-03 12:34:48 +0300

    done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container, HomeImg, ImgDiv } from "./About.style";
+import IMG_0530 from "../../assets/IMG_0530.JPG";
+import IMG_0338 from "../../assets/IMG_0338.JPG";
+import AGZA6920 from "../../assets/AGZA6920.JPG";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Container>
+      <p>We like eating so much. We are happy while eating something</p>
 
-export default About
+      <ImgDiv>
+        <HomeImg src={IMG_0530} />
+        <HomeImg src={AGZA6920} />
+        <HomeImg src={IMG_0338} />
+      </ImgDiv>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/pages/about/About.style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ImgDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 5rem;
+`;
+
+export const HomeImg = styled.img`
+  width: 60%;
+  max-width: 500px;
+  /* -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+`;
+export const Container = styled.div`
+  text-align: center;
+  padding-top: 5rem;
+`;
