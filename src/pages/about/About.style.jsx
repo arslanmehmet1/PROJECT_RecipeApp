2f5582f10da05ae40991fe4ce0a8f1afc49876f6
Mehmet Arslan <marslan.developer@gmail.com>
2022-11-03 12:35:46 +0300

    done

--- src/pages/about/About.style.jsx
+++ src/pages/about/About.style.jsx
@@ -10,7 +10,7 @@ export const HomeImg = styled.img`
   width: 60%;
   max-width: 500px;
   /* -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
-  filter: grayscale(100%);
+  /* filter: grayscale(100%); */
 `;
 export const Container = styled.div`
   text-align: center;
